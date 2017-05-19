a761ef39d1ddd532a50c641f2f8a3e6fcc3fa919
Ole Rohne <ole.rohne@gmail.com>
2017-05-19 09:29:54 +0200

    For macOS build

---
+++ bundle.sh
@@ -0,0 +1,1 @@
+../../macdylibbundler/dylibbundler -x klayout.app/Contents/MacOS/klayout -b -d klayout.app/Contents/libs -od -cd
---
+++ settings.sh
@@ -0,0 +1,3 @@
+export PATH=/Applications/Xcode.app/Contents/Developer/usr/bin:$PATH
+export PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
+export PATH=/usr/local/opt/qt/bin:$PATH
