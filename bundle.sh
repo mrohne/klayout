33f7cd852049951b72859f44d7739798e4afc1e8
Ole Rohne <ole.rohne@gmail.com>
2017-05-26 06:44:57 +0200

    On branch local/build - corrected mac bundle fixup

--- bundle.sh
+++ bundle.sh
@@ -1,1 +1,12 @@
-../../macdylibbundler/dylibbundler -x klayout.app/Contents/MacOS/klayout -b -d klayout.app/Contents/libs -od -cd
+bundle=$1
+oldpath=/usr/local/Cellar/qt/5.8.0_2/lib/
+newpath=@executable_path/../Frameworks/
+macdeployqt $1 -no-strip -libpath=.
+find $1 -type f | 
+    while read file; do 
+	otool -h $file >/dev/null 2>&1 && otool -L $file 2>/dev/null | grep $oldpath | 
+		while read deps version; do 
+		    repl=$(echo $deps | sed -e "s#$oldpath#$newpath#");
+		    install_name_tool -change $deps $repl $file;
+		done;
+    done
