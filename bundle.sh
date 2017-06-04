6f8dd5d9807bc331179ed8ce00ba33111daff3d3
Ole Rohne <ole.rohne@gmail.com>
2017-06-04 22:02:08 +0200

    On branch local/build - updated to Qt 5.9.0

--- bundle.sh
+++ bundle.sh
@@ -1,5 +1,5 @@
 bundle=$1
-oldpath=/usr/local/Cellar/qt/5.8.0_2/lib/
+oldpath=/usr/local/Cellar/qt/5.9.0/lib/
 newpath=@executable_path/../Frameworks/
 macdeployqt $1 -no-strip -libpath=.
 find $1 -type f | 
